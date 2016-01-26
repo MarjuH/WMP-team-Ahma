a547de949090f6f0156be4e227292d49847ad320
MarjuH <marju.hanhij@gmail.com>
2016-01-26 10:23:04 +0200

    OL map new style

--- openlayers/js/map.js
+++ openlayers/js/map.js
@@ -1,3 +1,4 @@
+ /*
  var map = new ol.Map({
         target: 'map',
         layers: [
@@ -10,3 +11,25 @@
           zoom: 12
         })
       });
+*/	  
+	  
+	  
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.XYZ({
+        url: 'http://{1-4}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        attributions: [new ol.Attribution({ html: ['&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'] })]
+      })
+    })
+  ],
+	view: new ol.View({
+	  center: ol.proj.fromLonLat([24.955494, 60.18]),
+	  zoom: 13
+	})     
+});
+
+
+
+
