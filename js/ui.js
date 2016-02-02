9cdde9be0b9db58c4cf10b6fb781c4698a52c933
MarjuH <marju.hanhij@gmail.com>
2016-02-02 12:38:56 +0200

    OL UI

---
+++ js/ui.js
@@ -0,0 +1,17 @@
+ function onToggleMenu(){
+        $("#wrapper").toggleClass("toggled");
+    }
+	
+$(function () {
+	$('#datetimepicker1').datepicker({
+        format: 'dd.mm.yyyy'
+    });
+	
+});
+
+$(function () {
+	$('#datetimepicker2').datepicker({
+        format: 'dd.mm.yyyy'
+    });
+	
+});
