2663c74f9a7a97e46de5fca1116b76d8ea558002
obscurage <samuli.hyttinen@gmail.com>
2016-02-04 11:15:10 +0200

    Datepicker population

--- js/ui.js
+++ js/ui.js
@@ -1,17 +1,32 @@
- function onToggleMenu(){
-        $("#wrapper").toggleClass("toggled");
-    }
+function onToggleMenu(){
+	$("#wrapper").toggleClass("toggled");
+}
 	
 $(function () {
 	$('#datepicker-start').datepicker({
-        format: 'dd.mm.yyyy'
-    });
+		defaultDate: moment().subtract(1, 'months'),
+		locale: "fi",
+		format: "dd.mm.yyyy"
+	});
 	
 });
 
 $(function () {
 	$('#datepicker-end').datepicker({
-        format: 'dd.mm.yyyy'
-    });
+		defaultDate: moment(),
+		locale: "fi",
+		format: "dd.mm.yyyy"
+	});
 	
+	
+});
+
+
+$(document).ready(function() {
+	var startDate = moment().subtract(1, 'months');
+	$('#datepicker-start').datepicker('setDate', new Date(startDate));
+	$('#datepicker-start').datepicker('update');
+	
+	$('#datepicker-end').datepicker('setDate', new Date);
+	$('#datepicker-end').datepicker('update');
 });
